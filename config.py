d85680d0f12f4282c4f69d1299b47464d3a39df3
Juan Pablo <71467905+juanpaat@users.noreply.github.com>
2025-10-06 23:19:53 +0100

    Agregar informe a gitignore y agregar config

--- config.py
+++ config.py
@@ -13,4 +13,32 @@ meses_esp = {
     'Dec': 'Dic'
 }
 
-
+# Subareas for preventivos data processing
+subareas_preventivos = [
+    'Ubicación Bloque 1',
+    'Ubicación Bloque 2', 
+    'Ubicación Bloque 3',
+    'Ubicación Bloque 4',
+    'Ubicación Bloque 5 (verde)',
+    'Ubicación Bloque 6',
+    'Ubicación Bloque 7',
+    'Ubicación Bloque 8',
+    'Ubicación Bloque 9',
+    'Ubicación Bloque 10',
+    'Ubicación Bloque 11',
+    'Ubicación Bloque 12',
+    'Ubicación Bloque 13',
+    'Ubicación Bloque 14',
+    'Ubicación Bloque 15',
+    'Ubicación Bloque 16',
+    'Ubicación Bloque 17',
+    'Acopio de Basuras y portería',
+    'Plantas de Emergencias',
+    'Áreas Quirúrgicas',
+    'Cuartos técnicos y gases medicinales',
+    'Zona externa',
+    'Torre A',
+    'Torre B', 
+    'Torre C',
+    'Torre D'
+]
